57a140a90ef063a43ff07290355ec7c22e9429ea
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-27 21:45:51 -0400

    commit tree 868476fda8fd8ed86b747301691e334b36f340ee

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCats = [...cats,name];
+    return newCats;
+}
+function prependCat(name){
+    const lessCats = [name, ...cats];
+    return lessCats;
+}
+function removeLastCat(){
+    const oneLessCat = cats.slice(0,2);
+    return oneLessCat;
+}
+function removeFirstCat(){
+    const noFirstCat = cats.slice(-2);
+    return noFirstCat;
+}
